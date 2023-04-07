c59fcb3a7224c7ce0a98898a39792e62efcd0368
delta277 <72160172+delta-bravo277@users.noreply.github.com>
2023-04-07 19:25:13 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ minor_string_token=${MINOR_STRING_TOKEN:-#minor}
 patch_string_token=${PATCH_STRING_TOKEN:-#patch}
 none_string_token=${NONE_STRING_TOKEN:-#none}
 branch_history=${BRANCH_HISTORY:-compare}
-# since https://github.blog/2022-04-12-git-security-vulnerability-announced/ runner uses?
+
 git config --global --add safe.directory /github/workspace
 
 cd "${GITHUB_WORKSPACE}/${source}" || exit 1
