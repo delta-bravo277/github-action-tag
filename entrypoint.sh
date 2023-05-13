4bd803481d1cb32aa6508b9ac0988f455a504ab3
delta-bravo277 <sanketlawande1@gmail.com>
2023-05-14 00:38:06 +0530

    custom code CHANGED

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,10 +8,10 @@ default_branch=${DEFAULT_BRANCH:-$GITHUB_BASE_REF} # get the default branch from
 with_v=${WITH_V:-false}
 # release_branches=${RELEASE_BRANCHES:-master,main}
 # custom_tag=${CUSTOM_TAG:-}
-source=${SOURCE:-.}
+# source=${SOURCE:-.}
 # dryrun=${DRY_RUN:-false}
-initial_version=${INITIAL_VERSION:-0.0.0}
-tag_context=${TAG_CONTEXT:-repo}
+# initial_version=${INITIAL_VERSION:-0.0.0}
+# tag_context=${TAG_CONTEXT:-repo}
 # prerelease=${PRERELEASE:-false}
 # suffix=${PRERELEASE_SUFFIX:-beta}
 # verbose=${VERBOSE:-false}
@@ -19,7 +19,7 @@ major_string_token=${MAJOR_STRING_TOKEN:-#major}
 minor_string_token=${MINOR_STRING_TOKEN:-#minor}
 patch_string_token=${PATCH_STRING_TOKEN:-#patch}
 none_string_token=${NONE_STRING_TOKEN:-#none}
-branch_history=${BRANCH_HISTORY:-compare}
+# branch_history=${BRANCH_HISTORY:-compare}
 
 git config --global --add safe.directory /github/workspace
 
@@ -27,14 +27,14 @@ cd "${GITHUB_WORKSPACE}/${source}" || exit 1
 
 echo "*** CONFIGURATION ***"
 echo -e "\tDEFAULT_BUMP: ${default_semvar_bump}"
-echo -e "\tDEFAULT_BRANCH: ${default_branch}"
+# echo -e "\tDEFAULT_BRANCH: ${default_branch}"
 echo -e "\tWITH_V: ${with_v}"
 # echo -e "\tRELEASE_BRANCHES: ${release_branches}"
 # echo -e "\tCUSTOM_TAG: ${custom_tag}"
-echo -e "\tSOURCE: ${source}"
+# echo -e "\tSOURCE: ${source}"
 # echo -e "\tDRY_RUN: ${dryrun}"
-echo -e "\tINITIAL_VERSION: ${initial_version}"
-echo -e "\tTAG_CONTEXT: ${tag_context}"
+# echo -e "\tINITIAL_VERSION: ${initial_version}"
+# echo -e "\tTAG_CONTEXT: ${tag_context}"
 # echo -e "\tPRERELEASE: ${prerelease}"
 # echo -e "\tPRERELEASE_SUFFIX: ${suffix}"
 # echo -e "\tVERBOSE: ${verbose}"
@@ -42,7 +42,7 @@ echo -e "\tMAJOR_STRING_TOKEN: ${major_string_token}"
 echo -e "\tMINOR_STRING_TOKEN: ${minor_string_token}"
 echo -e "\tPATCH_STRING_TOKEN: ${patch_string_token}"
 echo -e "\tNONE_STRING_TOKEN: ${none_string_token}"
-echo -e "\tBRANCH_HISTORY: ${branch_history}"
+# echo -e "\tBRANCH_HISTORY: ${branch_history}"
 
 # # verbose, show everything
 # if $verbose
@@ -82,11 +82,11 @@ preTagFmt="^v?[0-9]+\.[0-9]+\.[0-9]+(-$suffix\.[0-9]+)$"
 case "$tag_context" in
     *repo*) 
         tag="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$tagFmt" | head -n 1)"
-        pre_tag="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt" | head -n 1)"
+        # pre_tag="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$preTagFmt" | head -n 1)"
         ;;
     *branch*) 
         tag="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$tagFmt" | head -n 1)"
-        pre_tag="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt" | head -n 1)"
+        # pre_tag="$(git tag --list --merged HEAD --sort=-v:refname | grep -E "$preTagFmt" | head -n 1)"
         ;;
     * ) echo "Unrecognised context"
         exit 1;;
@@ -101,15 +101,15 @@ then
     else
         tag="$initial_version"
     fi
-    if [ -z "$pre_tag" ] && $pre_release
-    then
-        if $with_v
-        then
-            pre_tag="v$initial_version"
-        else
-            pre_tag="$initial_version"
-        fi
-    fi
+    # if [ -z "$pre_tag" ] && $pre_release
+    # then
+    #     if $with_v
+    #     then
+    #         pre_tag="v$initial_version"
+    #     else
+    #         pre_tag="$initial_version"
+    #     fi
+    # fi
 fi
 
 # get current commit hash for tag
@@ -126,18 +126,18 @@ then
 fi
 
 # sanitize that the default_branch is set (via env var when running on PRs) else find it natively
-if [ -z "${default_branch}" ] && [ "$branch_history" == "full" ]
-then
-    echo "The DEFAULT_BRANCH should be autodetected when tag-action runs on on PRs else must be defined, See: https://github.com/anothrNick/github-tag-action/pull/230, since is not defined we find it natively"
-    default_branch=$(git branch -rl '*/master' '*/main' | cut -d / -f2)
-    echo "default_branch=${default_branch}"
-    # re check this
-    if [ -z "${default_branch}" ]
-    then
-        echo "::error::DEFAULT_BRANCH must not be null, something has gone wrong."
-        exit 1
-    fi
-fi
+# if [ -z "${default_branch}" ] && [ "$branch_history" == "full" ]
+# then
+#     # echo "The DEFAULT_BRANCH should be autodetected when tag-action runs on on PRs else must be defined, See: https://github.com/anothrNick/github-tag-action/pull/230, since is not defined we find it natively"
+#     # default_branch=$(git branch -rl '*/master' '*/main' | cut -d / -f2)
+#     echo "default_branch=${default_branch}"
+#     # re check this
+#     if [ -z "${default_branch}" ]
+#     then
+#         echo "::error::DEFAULT_BRANCH must not be null, something has gone wrong."
+#         exit 1
+#     fi
+# fi
 
 # get the merge commit message looking for #bumps
 declare -A history_type=( 
